3f8a3f78a7d2e4e9174b841968537e15643f58f2
notblondenough <128834513+notblondenough@users.noreply.github.com>
2024-01-11 11:52:21 +0530

    Update api_client.ts

--- frontend/src/api_client.ts
+++ frontend/src/api_client.ts
@@ -1,6 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://happyholidays.onrender.com";
 
 export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
